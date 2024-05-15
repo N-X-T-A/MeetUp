2020525f69181769b5889544512dd437b04d1922
ItBanz <catus0342@gmail.com>
2024-05-15 10:01:17 +0700

    Update-UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { RoomProvider } from "./context/RoomContext";
